b37d9d4c5fe318ae27b5bc7f6a7bbb810b967131
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGridCustomTheme/ThemingInlineEditors.xaml.cs
@@ -0,0 +1,53 @@
+﻿// Developer Express Code Central Example:
+// How to apply a custom theme to grid elements
+// 
+// The following example demonstrates how to create a custom theme and apply it to
+// the elements of the DXGrid control.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E1134
+
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Grid;
+using System.ComponentModel;
+
+namespace CustomTheme {
+
+    public partial class ThemingInlineEditors : UserControl {
+        BindingList<TestData> list;
+        public ThemingInlineEditors() {
+            InitializeComponent();
+            list = new BindingList<TestData>();
+            for(int i = 0; i < 1000; i++) {
+                list.Add(new TestData() 
+                    { Text = "row " + i, Number1 = i / 100, Number2 = i / 5 });
+            }
+            grid.DataSource = list;
+        }
+
+        private void navigationCheckBox_Checked(object sender, RoutedEventArgs e) {
+            view.NavigationStyle = GridViewNavigationStyle.Cell;
+        }
+
+        private void navigationCheckBox_Unchecked(object sender, RoutedEventArgs e) {
+            view.NavigationStyle = GridViewNavigationStyle.Row;
+            if(view.MultiSelectMode == TableViewSelectMode.Cell)
+                selectMode.SelectedIndex = 1;
+        }
+
+        private void newItemRowCheckBox_Checked(object sender, RoutedEventArgs e) {
+            view.NewItemRowPosition = NewItemRowPosition.Top;
+        }
+
+        private void newItemRowCheckBox_Unchecked(object sender, RoutedEventArgs e) {
+            view.NewItemRowPosition = NewItemRowPosition.None;
+        }
+
+        private void selectMode_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if((TableViewSelectMode)selectMode.SelectedItem == TableViewSelectMode.Cell)
+                navigationCheckBox.IsChecked = true;
+        }
+    }
+}
---
+++ VB/DXGridCustomTheme/Application.xaml.vb
@@ -0,0 +1,25 @@
+﻿' Developer Express Code Central Example:
+' How to apply a custom theme to grid elements
+' 
+' The following example demonstrates how to create a custom theme and apply it to
+' the elements of the DXGrid control.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E1134
+
+
+Imports Microsoft.VisualBasic
+Imports System.Windows
+
+Namespace CustomTheme
+
+	Partial Public Class App
+		Inherits Application
+	End Class
+
+	Public Class TestData
+		Public Property Text() As String
+		Public Property Number1() As Integer
+		Public Property Number2() As Integer
+	End Class
+End Namespace
