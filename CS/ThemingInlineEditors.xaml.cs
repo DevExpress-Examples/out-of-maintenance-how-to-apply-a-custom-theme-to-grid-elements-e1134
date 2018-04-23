a7b894cb645e7970e08ff95d3052f659295e94ad
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ThemingInlineEditors.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Grid;
+using System.ComponentModel;
+
+namespace CustomTheme {
+
+    public partial class ThemingInlineEditors : UserControl {
+        BindingList<TestData> list;
+        public ThemingInlineEditors() {
+            InitializeComponent();
+            list = new BindingList<TestData>();
+            for(int i = 0; i < 1000; i++) {
+                list.Add(new TestData() 
+                    { Text = "row " + i, Number1 = i / 100, Number2 = i / 5 });
+            }
+            grid.DataSource = list;
+        }
+
+        private void navigationCheckBox_Checked(object sender, RoutedEventArgs e) {
+            view.NavigationStyle = GridViewNavigationStyle.Cell;
+        }
+
+        private void navigationCheckBox_Unchecked(object sender, RoutedEventArgs e) {
+            view.NavigationStyle = GridViewNavigationStyle.Row;
+            if(view.MultiSelectMode == TableViewSelectMode.Cell)
+                selectMode.SelectedIndex = 1;
+        }
+
+        private void newItemRowCheckBox_Checked(object sender, RoutedEventArgs e) {
+            view.NewItemRowPosition = NewItemRowPosition.Top;
+        }
+
+        private void newItemRowCheckBox_Unchecked(object sender, RoutedEventArgs e) {
+            view.NewItemRowPosition = NewItemRowPosition.None;
+        }
+
+        private void selectMode_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if(view.MultiSelectMode == TableViewSelectMode.Cell) navigationCheckBox.IsChecked = true;
+        }
+    }
+}
---
+++ VB/AddNewTheme.xaml.vb
@@ -0,0 +1,47 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Collections.Generic
+Imports System.Windows
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Grid
+Imports System.ComponentModel
+
+Namespace CustomTheme
+
+	Partial Public Class AddNewTheme
+		Inherits UserControl
+		Private list As BindingList(Of TestData)
+		Public Sub New()
+			InitializeComponent()
+			list = New BindingList(Of TestData)()
+			For i As Integer = 0 To 999
+				list.Add(New TestData() With {.Text = "row " & i, .Number1 = i \ 100, .Number2 = i \ 5})
+			Next i
+			grid.DataSource = list
+		End Sub
+
+		Private Sub navigationCheckBox_Checked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			view.NavigationStyle = GridViewNavigationStyle.Cell
+		End Sub
+
+		Private Sub navigationCheckBox_Unchecked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			view.NavigationStyle = GridViewNavigationStyle.Row
+			If view.MultiSelectMode = TableViewSelectMode.Cell Then
+				selectMode.SelectedIndex = 1
+			End If
+		End Sub
+
+		Private Sub newItemRowCheckBox_Checked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			view.NewItemRowPosition = NewItemRowPosition.Top
+		End Sub
+
+		Private Sub newItemRowCheckBox_Unchecked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			view.NewItemRowPosition = NewItemRowPosition.None
+		End Sub
+
+		Private Sub selectMode_SelectionChanged(ByVal sender As Object, ByVal e As SelectionChangedEventArgs)
+			If view.MultiSelectMode = TableViewSelectMode.Cell Then
+				navigationCheckBox.IsChecked = True
+			End If
+		End Sub
+	End Class
+End Namespace
